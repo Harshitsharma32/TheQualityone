3d96855a7195c3204b459313c1f545b2e6ee539c
Harshitsharma32 <81152569+Harshitsharma32@users.noreply.github.com>
2021-07-25 17:32:41 +0530

    Add files via upload

---
+++ newslettter.php
@@ -0,0 +1,42 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFERUPDATES</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://www.facebook.com/ankit.raghav.90260403" target="_blank"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://www.instagram.com/raghavankitsingh0/" target="_blank"><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://twitter.com/AnkitRa26797676" target="_blank"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://www.linkedin.com/in/ankit-raghav-326703165" target="_blank" target="_blank"><i class="fa fa-linkedin"></i></a>
+								</li>
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
